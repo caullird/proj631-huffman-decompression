3d1e0d8b61727df5633d012078639d5bc8f48283
caullird <caullireau.dorian@gmail.com>
2021-05-27 16:47:05 +0200

    Update HuffmanTree.java

--- proj631-huffman-decompression/src/main_program/HuffmanTree.java
+++ proj631-huffman-decompression/src/main_program/HuffmanTree.java
@@ -1,5 +1,43 @@
 package main_program;
 
-public class HuffmanTree {
+import java.util.*;
 
+
+public class HuffmanTree {
+	
+
+	private HuffmanNode root;
+	private HuffmanNode current;
+
+	public HuffmanTree(Dictionary<?, ?> frequency) {
+
+		ArrayList<HuffmanNode> nodes = new ArrayList<>();
+		for(Enumeration<?> character = frequency.keys(); character.hasMoreElements(); ) {
+			String c = character.nextElement().toString();
+			nodes.add(new HuffmanNode(c, frequency.get(c).toString()));
+		}
+		
+		Collections.sort(nodes);
+		
+		while(nodes.size() > 1) {
+			nodes.add(new HuffmanNode(nodes.remove(0), nodes.remove(0)));		
+		}
+		
+		this.setRoot(nodes.get(0));
+		this.setCurrent(nodes.get(0));
+	}
+	
+	public HuffmanNode getCurrent() { return current; }
+	
+	public void getRootNode() { this.current = this.root; }
+	
+	public void getLeftNode() { this.current = this.current.getLeft(); }
+	
+	public void getRightNode() { this.current = this.current.getRight(); }
+	
+	public HuffmanNode getRoot() { return root; }
+
+	public void setRoot(HuffmanNode root) { this.root = root; }
+
+	public void setCurrent(HuffmanNode current) { this.current = current; }
 }
