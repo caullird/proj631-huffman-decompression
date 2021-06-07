9207e20df0d8acafbf1ab7acb1f1e099fe378f85
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-06-07 20:58:57 +0200

    Update HuffmanMain.java

--- proj631-huffman-decompression/src/HuffmanMain.java
+++ proj631-huffman-decompression/src/HuffmanMain.java
@@ -3,9 +3,7 @@ import java.io.FileNotFoundException;
 import main_program.HuffmanDecode;
 
 public class HuffmanMain {
-
 	public static void main(String[] args) throws FileNotFoundException {
-		new HuffmanDecode("src/data/initial_data/exemple_freq.txt","src/data/initial_data/exemple_comp.txt");
+		new HuffmanDecode("src/data/initial_data/ceci_freq.txt","src/data/initial_data/ceci_comp.txt");
 	}
-	
 }
