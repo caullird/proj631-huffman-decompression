9f94c47e840e9720996ade6a6fba0e96f934b11a
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-06-07 18:18:39 +0200

    Create HuffmanTraductionInterface.java

---
+++ proj631-huffman-decompression/src/main_program/Interface/HuffmanTraductionInterface.java
@@ -0,0 +1,14 @@
+package main_program.Interface;
+
+public interface HuffmanTraductionInterface {
+
+	/* BinaryStringTranslation() function
+	 * 
+	 * Allows to pass from a binary string to a character string with the dictionary 
+	 * 
+	 * @Override : BinaryStringTranslation() in HuffmanTraductionInterface
+	 * 
+	 */
+	
+	static void BinaryStringTranslation() {};
+}
