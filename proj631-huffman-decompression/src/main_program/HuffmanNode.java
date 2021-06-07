294ad3d8c98ec7902e03794c3262a7a011c93785
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-06-07 18:18:45 +0200

    Update HuffmanNode.java

--- proj631-huffman-decompression/src/main_program/HuffmanNode.java
+++ proj631-huffman-decompression/src/main_program/HuffmanNode.java
@@ -1,12 +1,31 @@
 package main_program;
 
-public class HuffmanNode implements Comparable<HuffmanNode> {
+import main_program.Interface.HuffmanNodeInterface;
+
+/* Class HuffmanNode 
+* 
+* Implements : HuffmanNodeInterface
+* 
+* Global node management
+* 
+*/
+
+public class HuffmanNode implements Comparable<HuffmanNode>, HuffmanNodeInterface {
 	
 	private char stringChar;
 	private int weight;
 
 	private HuffmanNode left;
 	private HuffmanNode right;
+	
+	/* Constructor of the HuffmanNode Class
+	 * 
+	 * Create HuffmanNode Object 
+	 * 
+	 * @Override constructor: Possibility to create a node 
+	 * via two child nodes, or via a character and a weight
+	 * 
+	 */
 	
 	public HuffmanNode(String character, String weight) {
 		this.stringChar = character.charAt(0);
@@ -20,16 +39,67 @@ public class HuffmanNode implements Comparable<HuffmanNode> {
 	}
 	
 	public void setChildren(HuffmanNode left, HuffmanNode right) {
+		
+		/* setChildren() function
+		 * 
+		 * Allows you to add child nodes to a parent node
+		 * 
+		 * @Override : setChildren() in FileInterface
+		 * 
+		 */
+		
 		this.setLeft(left);
 		this.setRight(right);
 		this.setWeight(left.getWeight() + right.getWeight());
 	}
 
 	@Override
-	public int compareTo(HuffmanNode unNode) { return this.weight - unNode.weight; }	
+	public int compareTo(HuffmanNode unNode) { 
+		
+		/* compareTo() function
+		 * 
+		 * Allows you to compare one node to another via their weight 
+		 * 
+		 * @Override : compareTo() in Comparable
+		 * 
+		 */
+		
+		return this.getWeight() - unNode.getWeight(); 
+	}	
 	
-	public boolean isLeaf() { return this.getRight() == null && this.getLeft() == null; }
-
+	public boolean isLeaf() { 
+		
+		/* isLeaf() function
+		 * 
+		 * Determines whether a node is a leaf or not
+		 * 
+		 * @Override : isLeaf() in FileInterface
+		 * 
+		 */
+		
+		return this.getRight() == null && this.getLeft() == null; 
+	}
+	
+	/* Getters & Setters
+	 * 
+	 * 
+	 * Function allowing the modification and recovery of the elements of the object
+	 * 
+     * Getters 
+     * ----------
+     *	- getStringChar() : is used to retrieve the stringChar element 
+	 *  - getWeight() : is used to retrieve the weight element
+	 * 	- getLeft() : is used to retried the left node element
+	 * 	- getRight() : is used to retried the right node element
+	 *  
+	 * Setters 
+	 * ----------
+	 *  - setLeft() : is used to modify the left node element
+	 *  - setRight() : is used to modify the right node element
+	 *  - setWeight() : is used to modify the the weight element
+	 * 
+	 */
+	
 	public char getStringChar() { return stringChar; }
 
 	public int getWeight() { return weight; }
