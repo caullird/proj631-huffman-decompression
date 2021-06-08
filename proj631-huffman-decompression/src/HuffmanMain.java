73098786b8a1a87b2cbcdafe311509fe6d4dbd1f
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-06-08 08:53:05 +0200

    Update HuffmanMain.java

--- proj631-huffman-decompression/src/HuffmanMain.java
+++ proj631-huffman-decompression/src/HuffmanMain.java
@@ -2,8 +2,25 @@ import java.io.FileNotFoundException;
 
 import main_program.HuffmanDecode;
 
+/* Class HuffmanMain 
+* 
+* 
+* File that allows to choose the files, the only file that can be modified by the user
+* 
+*/
+
 public class HuffmanMain {
 	public static void main(String[] args) throws FileNotFoundException {
-		new HuffmanDecode("src/data/initial_data/ceci_freq.txt","src/data/initial_data/ceci_comp.txt");
+		
+		/* Main function of the program
+		 * 
+		 * Add the frequency file and the binary translation, in the data files -> "data/initial_data"
+		 * 
+		 */
+		
+		String freqFile = "src/data/initial_data/poly_freq.txt";
+		String binaryFile = "src/data/initial_data/poly_comp.txt";
+		
+		new HuffmanDecode(freqFile,binaryFile);
 	}
 }
